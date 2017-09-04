32ea84d3f49cdf5a3d8029dddfa30e9e56a6e12c
Jonas Lindenskov Nielsen <jonas@zafepay.com>
2017-09-04 17:31:13 +0200

    Work done

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # lectio-api
+
+## routes
+beskeder
+    get
+
+besked
+    get
+    create
+    delete
+    reply
+
+skema
+    get
+
+assignments
+    get
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const scrapers = require('./lib/scrapers');
+
+
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
@@ -11,11 +14,16 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.json({
-        message: 'Hello World!'
+
+    return scrapers.test({
+        school_id: 518,
+        student_id: 17544500480
+    }).then(response => {
+        return res.json(response);
     });
-})
+});
 
 app.listen(port, function () {
     console.log(`'Example app listening on port ${port}!'`);
 });
+1
